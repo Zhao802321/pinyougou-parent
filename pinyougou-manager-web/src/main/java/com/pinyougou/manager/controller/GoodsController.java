fb42273de960d6e103533e50c32c44b33f708165
gangtiexia <913888523@qq.com>
2019-01-05 21:03:30 +0800

    第12天网页静态化解决方案-Freemarker

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
@@ -2,6 +2,7 @@ package com.pinyougou.manager.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import com.pinyougou.page.service.ItemPageService;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -148,4 +149,15 @@ public class GoodsController {
 	}
 
 
-}
+	@Reference(timeout = 50000)
+	private ItemPageService itemPageService;
+
+	/**
+	 * 生成静态页（测试）
+	 * @param goodsId
+	 */
+	@RequestMapping("/genHtml.do")
+	public void genHtml(Long goodsId){
+		itemPageService.genItemHtml(goodsId);
+	}
+	}
